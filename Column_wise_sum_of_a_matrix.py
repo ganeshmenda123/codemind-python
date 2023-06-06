6953a894b4c5da34e11a2b8c5072f66878027371
Mendaganesh <99869389+ganeshmenda123@users.noreply.github.com>
2023-06-06 10:13:24 +0530

    n,m=map(int,input().split()) lis=[] for i in range(n):     k=list(map(int,input().split()))     lis.append(k) m=[] for j in range(len(k)):     c=0     for k in range(len(lis)):         c+=lis[k][j]     m.append(c) print(*m)

---
+++ Column_wise_sum_of_a_matrix.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+lis=[]
+for i in range(n):
+    k=list(map(int,input().split()))
+    lis.append(k)
+m=[]
+for j in range(len(k)):
+    c=0
+    for k in range(len(lis)):
+        c+=lis[k][j]
+    m.append(c)
+print(*m)
